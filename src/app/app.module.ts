267f236d460ea5f0f3cebce4abd4a0f2032fd875
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-08-21 22:39:48 -0300

    estilizando alguns components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ButtonDestaqueComponent } from './components/button-destaque/button-destaque.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { ContentComponent } from './pages/content/content.component';
     BigCardComponent,
     SmallCardComponent,
     HomeComponent,
-    ContentComponent
+    ContentComponent,
+    FooterComponent,
+    ButtonDestaqueComponent
   ],
   imports: [
     BrowserModule,
